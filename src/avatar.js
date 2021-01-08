b8ad1b6ab429ddc3800d395ab4886633d2638b05
jawaka345 <lu.k@p2p.legal>
2021-01-08 11:21:44 +0100

    add some avatars. The avatars loader code has been simplified

--- src/avatar.js
+++ src/avatar.js
@@ -1,23 +1,28 @@
-avatars.push( new Image())
-avatars[0].src = "img/hen.png"
+// Avatars file names. It could be possible to load automatically all the images of a directory 'avatars'
+// Avatars can be png or svg. They are automatically resized when printed.
+avatars_file_names = [
+    "img/hen.png",
+    "img/pig.png",
+    "img/cow.png",
+    "img/horse.svg",
+    "img/duck.svg",
+    "img/frog.svg",
+    "img/racoon.png",
+    "img/baleine.png",
+    "img/ours.png",
+    "img/rabbit.png",
+    "img/lion.png"
+]
 
-avatars.push( new Image())
-avatars[1].src = "img/pig.png"
-
-avatars.push( new Image())
-avatars[2].src = "img/cow.png"
-
-avatars.push( new Image())
-avatars[3].src = "img/horse.svg"
-
-avatars.push( new Image())
-avatars[4].src = "img/duck.svg"
-
-avatars.push( new Image())
-avatars[5].src = "img/frog.svg"
+// avatars are loaded in the variable 'avatars'
+for (var file_name of avatars_file_names) {
+    var img = new Image()
+    img.src = file_name
+    avatars.push(img)
+}
 
 
-my_avatar = Math.floor(Math.random()*avatars.length)
+my_avatar = Math.floor(Math.random() * avatars.length)
 
 img_bank = new Image()
 img_bank.src = "img/bank.svg"
